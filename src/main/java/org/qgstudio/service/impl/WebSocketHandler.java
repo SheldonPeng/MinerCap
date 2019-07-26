e4e29f4f8fee0571815fef5d8e07ee11a2f24ad9
SheldonPeng <775887734@qq.comgit config --global user.email  775887734@qq.com>
2019-07-26 17:06:56 +0800

    更新socket主客户端的心跳检测以及socket各个小模块的心跳检测功能

---
+++ src/main/java/org/qgstudio/service/impl/WebSocketHandler.java
@@ -0,0 +1,107 @@
+package org.qgstudio.service.impl;
+
+import org.qgstudio.utils.SocketClientPool;
+import org.qgstudio.service.MessageService;
+import org.qgstudio.service.impl.MessageServiceImpl;
+import org.qgstudio.utils.WebsocketClientPool;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+
+
+/**
+ * @Description: websocket的相关处理类
+ * @Param:
+ * @return:
+ * @Author: SheldonPeng
+ * @Date: 2019-07-25
+ */
+@Service
+public class WebSocketHandler extends TextWebSocketHandler {
+
+
+    private WebsocketClientPool websocketClientPool = WebsocketClientPool.getClientPool();
+
+    private SocketClientPool socketClientPool = SocketClientPool.getClientPool();
+
+    // 无帽子在线的字符串常量
+    private final String ZeroCapConnection = "当前没有嵌入式设备在线!";
+
+    private MessageService messageService = new MessageServiceImpl();
+
+    /**
+     * @Description: 当新连接建立完成之后，储存连接用户的session
+     * @Param: [session]
+     * @return: void
+     * @Author: SheldonPeng
+     * @Date: 2019-07-25
+     */
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+
+
+        System.out.println(session.getRemoteAddress().getAddress().getHostName() + "已连接");
+
+        websocketClientPool.addClient(session);
+
+        if ( socketClientPool.getClientCount() == 0){
+            session.sendMessage(new TextMessage(ZeroCapConnection));
+        } else {
+
+            session.sendMessage(new TextMessage("当前有" + socketClientPool.getClientCount() +"个设备在线!"));
+        }
+
+
+    }
+    /**
+     * @Description: 接收到来自安卓客户端的消息
+     * @Param: [session, message]
+     * @return: void
+     * @Author: SheldonPeng
+     * @Date: 2019-07-25
+     */
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
+        System.out.println(session.getRemoteAddress().getAddress().getHostName() + message.toString());
+
+        messageService.sendMsgToSocket(message.getPayload());
+
+    }
+
+    /**
+     * @Description: 处理与安卓客户端连接断开
+     * @Param: [session, status]
+     * @return: void
+     * @Author: SheldonPeng
+     * @Date: 2019-07-25
+     */
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        try {
+            websocketClientPool.exitClient(session);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+
+        System.out.println("error");
+        websocketClientPool.exitClient(session);
+    }
+
+}
---
+++ src/main/java/org/qgstudio/utils/CapPingPongUtil.java
@@ -0,0 +1,56 @@
+package org.qgstudio.utils;
+
+import org.qgstudio.service.MessageService;
+import org.qgstudio.service.impl.MessageServiceImpl;
+
+import java.nio.channels.SocketChannel;
+import java.security.PrivateKey;
+import java.util.Map;
+import java.util.TimerTask;
+
+/**
+ * @Description: 用于检测socket是否处于活跃状态
+ * @Param:
+ * @return:
+ * @Author: SheldonPeng
+ * @Date: 2019-07-26
+ */
+public class CapPingPongUtil extends TimerTask {
+
+    // 获取socket(嵌入式)端的用户连接池
+    private SocketClientPool socketClientPool = SocketClientPool.getClientPool();
+
+
+    private MessageService messageService = new MessageServiceImpl();
+
+    // 超时时间
+    private final int TIME_OUT = 1000*7;
+
+    // socket客户端上次活跃时间记录
+    private static  volatile Map<SocketChannel,Long>timeMileMap;
+
+    // 小模块的活跃状态集合
+    private static volatile Map<String,Long>modelTimeMileMap;
+
+    public CapPingPongUtil(Map<SocketChannel,Long>timeMileMap , Map<String,Long>modelTimeMileMap){
+
+        this.timeMileMap = timeMileMap;
+        this.modelTimeMileMap = modelTimeMileMap;
+    }
+
+    @Override
+    public void run() {
+
+        for (SocketChannel channel :
+                timeMileMap.keySet()) {
+            if (System.currentTimeMillis() - timeMileMap.get(channel) > TIME_OUT) {
+
+                System.out.println("嵌入式设备下线");
+                messageService.sendMsgToWebSocket("嵌入式设备下线");
+                socketClientPool.exitClient(channel);
+                timeMileMap.remove(channel);
+                modelTimeMileMap.clear();
+            }
+        }
+    }
+}
